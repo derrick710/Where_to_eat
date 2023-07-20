a8da9b285c6173255110863e7bf5955ed604d3ec
derrick710 <qqn@case.edu>
2023-07-20 16:23:05 +0700

    delete duplicate

--- app.py
+++ app.py
@@ -1,72 +1,18 @@
-from flask import Flask, request
-import pickle
-import pandas as pd
-from nltk.corpus import stopwords 
-import nltk
-import string
-import numpy as np
-import json
-import requests
+from yelp_api import YelpDataRetriever
+from data_cleaning import YelpDataProcessor
+from mssql import DataInsertion
 
-nltk.download('stopwords')
+### RETRIEVE DATA FROM YELP API + UP TO MONGO
+yelp_retriever = YelpDataRetriever()
+yelp_retriever.retrieve_business()
+#yelp_retriever.up_to_mongo()
 
-# Load ML model
-P, Q, userid_vectorizer = pickle.load(open("./model/yelp_recommendation_model_8.pkl", "rb"))
-
-# Setup text processing
-stop = []
-for word in stopwords.words('english'):
-    s = [char for char in word if char not in string.punctuation]
-    stop.append(''.join(s))
-
-api = Flask(__name__)
-
-api_key = "h56sDBVwv3kSJVYzUovbaR-uejvYqdorJL-wvrhJHCytcl0tCw3DDZtIBHLUScuXjZGNSmIs6VV9l96Gr1huG05MOrjSDj5N20UVRx8PQHHa4AjMyKNV0KKYLuTfY3Yx"
-
-# Class to represent a restaurant
-class Restaurant:
-    def __init__(self, id, name, categories, stars, reviews, image, url):
-        self.id = id
-        self.name = name
-        self.categories = [x['title'] + ", " for x in categories]
-        self.stars = stars
-        self.reviews = reviews
-        self.image = image
-        self.url = url
-    
-    def to_dict(self):
-        return {"id": self.id, "name": self.name, "categories": self.categories, "stars": self.stars, "reviews": self.reviews, "image": self.image, "url": self.url}
-
-# Processes inputted text for the ML model
-def text_process(mess):
-    nopunc = [char for char in mess if char not in string.punctuation]
-    nopunc = ''.join(nopunc)
-    return " ".join([word for word in nopunc.split() if word.lower() not in stop])
-
-# Creates a list of restaurants from a dataframe
-def build_results(df):
-    restaurants = []
-    headers = {"accept": "application/json", "Authorization": "Bearer {}".format(api_key)}
-    for id in df.index:
-        url = "https://api.yelp.com/v3/businesses/" + id
-        response = requests.get(url, headers=headers).json()
-        restaurants.append(Restaurant(response['id'],
-                                      response['name'],
-                                      response['categories'],
-                                      str(response['rating']),
-                                      str(response['review_count']),
-                                      response['image_url'],
-                                      response['url']
-                                     ))
-    return restaurants
-
-@api.route('/search', methods=['POST'])
-def generate_recommendations():
-    words = request.json["words"]
-    test_df= pd.DataFrame([words], columns=['text'])
-    test_df['text'] = test_df['text'].apply(text_process)
-    test_vectors = userid_vectorizer.transform(test_df['text'])
-    test_v_df = pd.DataFrame(test_vectors.toarray(), index=test_df.index, columns=userid_vectorizer.get_feature_names_out())
-    predictItemRating=pd.DataFrame(np.dot(test_v_df.loc[0],Q.T),index=Q.index,columns=['Rating'])
-    topRecommendations=pd.DataFrame.sort_values(predictItemRating,['Rating'],ascending=[0])[:5]
-    return json.dumps({"results": [obj.to_dict() for obj in build_results(topRecommendations)]})
+### CLEAN DATA
+yelp_processor = YelpDataProcessor()
+yelp_processor.process_data(yelp_retriever.get_business_json())
+business_df_clean = yelp_processor.get_processed_data()
+### UPLOAD TO MSSQL
+#msinsert = DataInsertion()
+#print(business_df_clean['name'])
+DataInsertion().insert_business_to_mssql(business_df_clean)
+DataInsertion().get_reviews(business_df_clean)
--- main.py
+++ main.py
@@ -1,67 +1,6 @@
-from flask import Flask, render_template, request
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-import numpy as np
-from mssql import DataInsertion
 import string
 from nltk.corpus import stopwords
 from nltk.tokenize import WordPunctTokenizer
-
-app = Flask(__name__)
-
-# Load data from SQL server
-review = pd.DataFrame(DataInsertion().query('select * from review'))
-business = pd.DataFrame(DataInsertion().query('select * from business'))
-business.columns = ['id', 'alias', 'name', 'url', 'eview_count', 'phone', 'distance', 'latitude', 'longitude', 'rating', 'categories', 'transactions']
-review.columns = ['review_id', 'text', 'rating','time_created','user_id', 'business_id']
-business.dropna(inplace=True)
-review.dropna(inplace=True)
-review_user = review[['business_id', 'text', 'rating', 'user_id']]
-
-# Define stopwords and punctuation
-stop = set(stopwords.words('english'))
-punctuation = set(string.punctuation)
-
-# Define function to remove stopwords and punctuation
-def remove_stopwords_and_punctuation(text):
-    return " ".join([word for word in text.split() if word.lower() not in stop and word not in punctuation])
-
-# Process user and business text data
-user_text = review_user[['user_id', 'text']].groupby('user_id').agg(lambda x: " ".join(x))
-user_text['text'] = user_text['text'].apply(remove_stopwords_and_punctuation)
-business_text = review_user[['business_id', 'text']].groupby('business_id').agg(lambda x: " ".join(x))
-business_text['text'] = business_text['text'].apply(remove_stopwords_and_punctuation)
-
-# Create TF-IDF vectors for user and business text data
-userid_vectorizer = TfidfVectorizer(tokenizer=WordPunctTokenizer().tokenize, max_features=5000)
-userid_vectors = userid_vectorizer.fit_transform(user_text['text'])
-businessid_vectorizer = TfidfVectorizer(tokenizer=WordPunctTokenizer().tokenize, max_features=5000)
-businessid_vectors = businessid_vectorizer.fit_transform(business_text['text'])
-
-# Create pivot table for user-item ratings
-userid_rating_matrix = pd.pivot_table(review_user, values='rating', index=['user_id'], columns=['business_id'])
-
-# Initialize P and Q matrices
-P = pd.DataFrame(userid_vectors.toarray(), index=user_text.index, columns=userid_vectorizer.get_feature_names_out())
-Q = pd.DataFrame(businessid_vectors.toarray(), index=business_text.index, columns=businessid_vectorizer.get_feature_names_out())
-
-# Define function for matrix factorization
-def matrix_factorization(R, P, Q, steps, gamma=0.001, lamda=0.02):
-    for i in range(steps):
-        for i in R.index:
-            for j in R.columns:
-                if R.loc[i, j] > 0:
-                    eij = R.loc[i, j] - np.dot(P.loc[i], Q.loc[j])
-                    P.loc[i] += gamma * (eij * Q.loc[j] - lamda * P.loc[i])
-                    Q.loc[j] += gamma * (eij * P.loc[i] - lamda * Q.loc[j])
-        e = 0
-        for i in R.index:
-            for j in R.columns:
-                if R.loc[i, j] > 0:
-                    e += pow(R.loc[i, j] - np.dot(P.loc[i], Q.loc[j]), 2) + lamda * (pow(np.linalg.norm(P.loc[i]), 2) + pow(np.linalg.norm(Q.loc[j]), 2))
-        if e < 0.001:
-            break
-    print(f"Step: {i}")
 from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
